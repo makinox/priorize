94bcb5bc253e1837b39766da9ae1b77e1d004b30
Jesus bossa <x@jesusbossa.com>
2019-10-31 23:34:18 -0500

    [IMPROVEMENT] Navbar layout styles improved

--- src/components/navbar/navbarLayout.js
+++ src/components/navbar/navbarLayout.js
@@ -5,7 +5,7 @@ export default (props) => (
   <header>
     <nav>
       <div>
-        <a href='/' style={{fontSize: '20px'}}>Priorize <span style={{fontSize: '15px'}}>{props.counter}</span></a>
+        <a href='/'>Priorize <span>{props.counter}</span></a>
       </div>
       <div>
         <a target="_blank" rel="noopener noreferrer" href='http://jesusbossa.com'>Me</a>
